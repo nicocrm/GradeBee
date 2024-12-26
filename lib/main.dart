a58f52eba0529c51e12e4f6934141d5fe8e1cc5f
Nicolas Galler <ngaller@4gclinical.com>
2024-12-26 10:05:09 +0100

    Class list vm

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:class_database/data/services/database.dart';
 import 'package:class_database/ui/class_list/class_list_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -11,8 +12,21 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: ClassListScreen()
-    );
+    const myApp = MaterialApp(home: ClassListScreen());
+    return _EagerLoading(child: myApp);
+  }
+}
+
+class _EagerLoading extends ConsumerWidget {
+  const _EagerLoading({required this.child});
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final provider = ref.watch(databaseProvider);
+    return provider.when(
+        loading: () => const Center(child: CircularProgressIndicator()),
+        error: (error, stackTrace) => Center(child: Text(error.toString())),
+        data: (data) => child);
   }
 }
