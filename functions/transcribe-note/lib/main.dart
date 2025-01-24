c90f4709582b0d36897e16c20644bd686da80ab2
Nicolas Galler <ngaller@4gclinical.com>
2025-01-24 09:01:44 +0100

    Start transcribe function

--- functions/transcribe-note/lib/main.dart
+++ functions/transcribe-note/lib/main.dart
@@ -2,11 +2,10 @@ import 'dart:async';
 import 'dart:io';
 import 'package:dart_appwrite/dart_appwrite.dart';
 import 'package:gradebee_models/common.dart';
-import 'note_splitter.dart';
 
-/// This Appwrite function will be executed whenever a note is created or updated with a "text" field.
-/// It will split the note into individual notes for each student and save them to the database.
-/// The "isSplit" field will be set to true.
+/// This Appwrite function will be executed whenever a note is created.
+/// It will transcribe the note's audio into the text field and save it to the database.
+/// The "isTranscribed" field will be set to true.
 Future<dynamic> main(final context) async {
   try {
     final client = Client()
@@ -15,29 +14,26 @@ Future<dynamic> main(final context) async {
         .setProject(Platform.environment['APPWRITE_FUNCTION_PROJECT_ID'] ?? '')
         .setKey(context.req.headers['x-appwrite-key'] ?? '');
     final note = Note.fromJson(context.req.bodyJson);
-    if (note.isSplit) {
+    if (note.isTranscribed) {
       return context.res
-          .json({'status': 'ignored', 'reason': 'Note is already split'});
+          .json({'status': 'ignored', 'reason': 'Note is already transcribed'});
     }
-    if (note.text == null) {
+    if (note.voice == null) {
       return context.res.json(
-          {'status': 'ignored', 'reason': 'Note does not have a text field'});
+          {'status': 'ignored', 'reason': 'Note does not have a voice field'});
     }
-    final splitter = NoteSplitter(Platform.environment['OPENAI_API_KEY'] ?? '');
-    final studentNotes = await splitter.splitNotesByStudent(note).toList();
+    String text = "This is a transcribed note";
     await Databases(client).updateDocument(
         databaseId: Platform.environment['APPWRITE_DATABASE_ID']!,
         collectionId: "notes",
         documentId: note.id,
         data: {
-          "is_split": true,
-          "student_notes": studentNotes.map((e) => e.toJson()).toList()
+          "is_transcribed": true,
+          "text": text
+          // "student_notes": studentNotes.map((e) => e.toJson()).toList()
         });
 
-    return context.res.json({
-      'status': 'success',
-      'student_notes': studentNotes.map((e) => e.toJson()).toList()
-    });
+    return context.res.json({'status': 'success', 'text': text});
   } catch (e, s) {
     context.error('${e.toString()}\n$s');
     return context.res.json({
