3682f4b5f1b42fe51d1e76c14bac5ffa97a4b4e4
Nicolas Galler <ngaller@4gclinical.com>
2025-02-15 08:10:10 +0100

    Start report card handler

--- functions/create-report-card/lib/report_card_generator.dart
+++ functions/create-report-card/lib/report_card_generator.dart
@@ -1,7 +1,20 @@
 import 'package:dart_openai/dart_openai.dart';
+import 'package:gradebee_models/common.dart';
 
 class ReportCardGenerator {
   ReportCardGenerator(String apiKey) {
     OpenAI.apiKey = apiKey;
+  }
+
+  Future<ReportCard> generateReportCard(ReportCard reportCard) async {
+    final response = await OpenAI.instance.chat.create(
+      model: "gpt-4o-mini",
+      messages: [
+        OpenAIChatCompletionChoiceMessageModel(
+          role: OpenAIChatMessageRole.system,
+          content: "You are a helpful assistant that generates report cards.",
+        ),
+      ],
+    );
   }
 }
