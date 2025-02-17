9913a51da84dda8279d6777d393ae7c8a8b918bf
Nicolas Galler <ngaller@gmail.com>
2025-02-17 22:49:07 +0100

    add isTranscribed attribute

--- functions/gradebee_models/lib/src/models/note.model.dart
+++ functions/gradebee_models/lib/src/models/note.model.dart
@@ -5,18 +5,24 @@ class Note {
   final Class class_;
   final String id;
   final DateTime when;
+  final bool isSplit;
+  final bool isTranscribed;
 
   Note(
       {required this.text,
       required this.class_,
       required this.id,
-      required this.when});
+      required this.when,
+      this.isSplit = false,
+      this.isTranscribed = false});
 
   factory Note.fromJson(Map<String, dynamic> json) {
     return Note(
         text: json["text"],
         class_: Class.fromJson(json["class"]),
         id: json["\$id"],
-        when: DateTime.parse(json["when"]));
+        when: DateTime.parse(json["when"]),
+        isSplit: json["is_split"],
+        isTranscribed: json["is_transcribed"]);
   }
 }
