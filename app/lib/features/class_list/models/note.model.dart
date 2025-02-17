acca2fa480f97875db7d4bbf7996e54072c5d0b8
Nicolas Galler <ngaller@gmail.com>
2025-02-17 22:49:07 +0100

    add is_split attribute + some scaffolding

--- app/lib/features/class_list/models/note.model.dart
+++ app/lib/features/class_list/models/note.model.dart
@@ -1,11 +1,17 @@
 class Note {
   final String text;
+  final bool isSplit;
   final String? id;
 
-  Note({required this.text, this.id});
+  Note({
+    required this.text,
+    this.id,
+    this.isSplit = false,
+  });
 
   factory Note.fromJson(Map<String, dynamic> json) {
-    return Note(text: json["text"], id: json["\$id"]);
+    return Note(
+        text: json["text"], id: json["\$id"], isSplit: json["is_split"]);
   }
 
   Map<String, dynamic> toJson() {
