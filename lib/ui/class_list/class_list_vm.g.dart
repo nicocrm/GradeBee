70438fb5f22dcd6854a54ac3286744cd595a9639
Nicolas Galler <ngaller@gmail.com>
2025-02-17 22:46:29 +0100

    Initial.  Firebase config.

---
+++ lib/ui/class_list/class_list_vm.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'class_list_vm.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$fetchClassesHash() => r'8df751ff7180cfc06a5ee7a33a609d4092e2dc4d';
+
+/// See also [fetchClasses].
+@ProviderFor(fetchClasses)
+final fetchClassesProvider = AutoDisposeStreamProvider<List<Class>>.internal(
+  fetchClasses,
+  name: r'fetchClassesProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$fetchClassesHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef FetchClassesRef = AutoDisposeStreamProviderRef<List<Class>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
